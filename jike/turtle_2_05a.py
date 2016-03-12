99cd6e225d946481dddef782eb6b113925e6ffb0
tgck <taniguchiko@gmail.com>
2016-03-13 04:12:22 +0900

    add helper

--- jike/turtle_2_05a.py
+++ jike/turtle_2_05a.py
@@ -7,7 +7,39 @@ from turtle import *
 def setups():
 	print getshapes()
 	setup (width=600, height=600, startx=830, starty=32)
+	setundobuffer(40)
 	reset()
+	ht()
+
+def guide():
+	st()
+	pensize(0.5)
+	color("gray")
+	penup()
+	goto(-300,0)
+	print "x:%d y:%d" % (xcor(), ycor())
+	pendown()
+	fd(600)
+	print "x:%d y:%d" % (xcor(), ycor())
+	penup()
+	setpos(0, -300)
+	pendown()
+	print "x:%d y:%d" % (xcor(), ycor())
+	goto(0, 300)
+	print "x:%d y:%d" % (xcor(), ycor())
+	goto(0, 0)
+
+	print shapesize()
+	shape("square")
+	pen(fillcolor = "")	
+	for i in range(1,10): # 10掛された値が実際の寸法になるらしい
+		shapesize(i)
+		stamp()
+	shapesize(20)
+	color("green")
+	pen(fillcolor = "")	
+	stamp()
+	fd(1)
 	ht()
 
 def main():
@@ -27,20 +59,27 @@ def main():
 def draw(str, vec, color):
 	goto(vec)
 	for i in range(8):
-		pendown()
-		dot(color)
+		pendown();
+		dot(color);
 		penup()
 		fd(30)
+	for i in range(4*4):
+		undo() # undo の単位は code step 
 	return
+
 
 if __name__ == '__main__':
 	setups()
-	main()
+	guide()
+	#main()
+	draw("dummy",Vec2D(0, 0), "black")
 	draw("dummy",Vec2D(0, 40), "red")
 	draw("dummy",Vec2D(0, 80), "green")
 	draw("dummy",Vec2D(0, 40), "blue")
 	draw("dummy",Vec2D(0, 40), "red")
+	ontimer(bye, 3388)
 	mainloop()
+
 
 
 
