8c076bc57939137bc4663fd3c889455a159724c5
tgck <taniguchiko@gmail.com>
2017-07-28 22:36:05 +0900

    * .

---
+++ peakie/README.md
@@ -0,0 +1,5 @@
+Peakie
+===
+p5.js environment on Webpack.
+
+![](./assets/peakie001.png)
--- peakie/my-project/src/main.js
+++ peakie/my-project/src/main.js
@@ -15,19 +15,34 @@ import p5 from 'p5'
 
 
 new p5(($)=>{
+  var counter = 0;
+  var k = 10.0;
+
   $.setup = () => {
+    // $.createCanvas(300, 300, $.WEBGL);
     $.createCanvas(300, 300);
   };
   $.draw = () => {
-    $.background(128, 30);
+
+    $.background(24, 188);
 
     $.stroke(0, 50);
     $.line(0,0,$.mouseX,$.mouseY);
     $.ellipse($.mouseX, $.mouseY, 10, 10);
 
-    $.stroke(0, 228, 48);
+    $.push();
+    $.shearX(0.05* $.sin(k*counter*0.1));
+    $.shearX(0.05* $.sin(k*counter*0.03));
+    $.shearY(0.05* $.cos(k*counter*0.1));
+    $.scale(1+ 0.5* $.sin(k*counter*0.005));
+    // $.rotateX(counter*0.01); // WEBGL
+    // $.rotateY(counter*0.001); // WEBGL
+    //$.translate(counter*0.1, 0, 0);
+    $.stroke(0, 228, 48, 255);
     $.noFill();
     $.ellipse($.mouseX, $.mouseY, 80, 80);
+    $.pop();
+    counter++;
   }
 
   // NOTE: 座標は取れている
